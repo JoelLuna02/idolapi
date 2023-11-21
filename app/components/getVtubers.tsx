862c456318571d4f76b5fc1061d70f957c7b79ee
JoelLuna02 <hectorjoelluna@outlook.com.ar>
2023-11-20 23:05:31 -0300

    Initial commit from Next

--- app/components/getVtubers.tsx
+++ app/components/getVtubers.tsx
@@ -80,7 +80,7 @@ export default async function VTubers() {
                             </div>
                             <p style={{ lineHeight: 1.3, fontSize: '0.95rem' }}>
                               <span className="fw-bold">Debut date: </span>
-                              { format(new Date(`${vtuber.debut}`), 'EEEE d MMMM HH:mm:ss') }
+                              { format(new Date(`${vtuber.debut}`), 'EEEE d MMMM yyyy HH:mm:ss') }
                               <br></br>
                               <span className="fw-bold">Branch</span>: {vtuber.branch}<br></br>
                               <span className="fw-bold">Unit</span>: {vtuber.unit}<br></br>
