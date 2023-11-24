e4e530a585076d8942a0a32823679a70dcc37cb6
JoelLuna02 <hectorjoelluna@outlook.com.ar>
2023-11-24 14:30:23 -0300

    Initial commit

--- next.config.js
+++ next.config.js
@@ -3,13 +3,20 @@ const withMDX = require('@next/mdx')(
 )
 
 const nextConfig = {
-    output: 'export',
+    // output: 'export',
     images: {
         domains: ['localhost', '127.0.0.1'],
         unoptimized: true
     },
-    distDir: 'dist',
+    // distDir: 'dist',
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+        // Important: return the modified config
+        return config
+    },
 }
 
 module.exports = withMDX(nextConfig)
